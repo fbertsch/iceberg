b0fb555119ea5767b4eb3db8c468b7b250a9e4bf
Rakesh Veeramacheneni <rveeramacheneni@netflix.com>
2023-02-14 01:20:07 -0800

    NETFLIX-BUILD: Add Namespace API support to MetacatIcebergCatalog

--- metacat/src/main/java/com/netflix/iceberg/metacat/MetacatIcebergCatalog.java
+++ metacat/src/main/java/com/netflix/iceberg/metacat/MetacatIcebergCatalog.java
@@ -3,10 +3,21 @@ package com.netflix.iceberg.metacat;
 import com.netflix.iceberg.CreateMADSnapshotListener;
 import com.netflix.iceberg.KSGatewayListener;
 import com.netflix.metacat.client.Client;
+import com.netflix.metacat.common.dto.DatabaseCreateRequestDto;
+import com.netflix.metacat.common.dto.DatabaseDto;
 import com.netflix.metacat.common.exception.MetacatAlreadyExistsException;
 import com.netflix.metacat.common.exception.MetacatNotFoundException;
+
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.iceberg.BaseMetastoreCatalog;
 import org.apache.iceberg.PartitionSpec;
@@ -15,16 +26,22 @@ import org.apache.iceberg.Table;
 import org.apache.iceberg.TableOperations;
 import org.apache.iceberg.TableProperties;
 import org.apache.iceberg.catalog.Namespace;
+import org.apache.iceberg.catalog.SupportsNamespaces;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.exceptions.AlreadyExistsException;
+import org.apache.iceberg.exceptions.NamespaceNotEmptyException;
+import org.apache.iceberg.exceptions.NoSuchNamespaceException;
 import org.apache.iceberg.exceptions.NoSuchTableException;
 import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
-import org.apache.iceberg.relocated.com.google.common.collect.ImmutableList;
 import org.apache.iceberg.relocated.com.google.common.collect.ImmutableMap;
 
-public class MetacatIcebergCatalog extends BaseMetastoreCatalog {
+public class MetacatIcebergCatalog extends BaseMetastoreCatalog implements SupportsNamespaces {
 
   private static volatile boolean initialized = false;
+
+  // Database Metadata property engines usually set
+  // when the DB has to be created in a specific location.
+  private static final String DB_LOCATION = "location";
 
   private static void initialize(String appName, String appId, Configuration conf) {
     if (!MetacatIcebergCatalog.initialized) {
@@ -130,7 +147,14 @@ public class MetacatIcebergCatalog extends BaseMetastoreCatalog {
 
   @Override
   public List<TableIdentifier> listTables(Namespace namespace) {
-    return ImmutableList.of();
+    Preconditions.checkArgument(namespace.levels().length == 2,
+            "Catalog and database name required to list tables: %s", namespace);
+    String catalogName = namespace.level(0);
+    String dbName = namespace.level(1);
+    return dbClient.getApi().getDatabase(catalogName, dbName,
+            false /* Include user-metadata */,
+            true /* Include table names */)
+      .getTables().stream().map(t -> TableIdentifier.of(namespace, t)).collect(Collectors.toList());
   }
 
   @Override
@@ -146,5 +170,121 @@ public class MetacatIcebergCatalog extends BaseMetastoreCatalog {
 
   private Client newClient() {
     return MetacatUtil.newClient(appName, metacatHost, conf);
+  }
+
+  @Override
+  public void createNamespace(Namespace namespace, Map<String, String> metadata) {
+    Preconditions.checkArgument(namespace.levels().length == 2,
+            "Invalid database name: %s", namespace);
+
+    String catalogName = namespace.level(0);
+    String dbName = namespace.level(1);
+    ImmutableMap.Builder<String, String> metadataBuilder = new ImmutableMap.Builder<>();
+    for (Map.Entry<String, String> entry : metadata.entrySet()) {
+      if (DB_LOCATION.equals(entry.getKey())) {
+        // TODO: See if we can update metacat to support this.
+        throw new UnsupportedOperationException("Metacat doesn't support setting database location");
+      } else {
+        metadataBuilder.put(entry.getKey(), entry.getValue());
+      }
+    }
+
+    try {
+      DatabaseCreateRequestDto createRequestDto = DatabaseCreateRequestDto.builder()
+        .metadata(metadataBuilder.build())
+        .build();
+      dbClient.getApi().createDatabase(catalogName, dbName, createRequestDto);
+    } catch (MetacatAlreadyExistsException e) {
+      throw new AlreadyExistsException("Namespace: %s already exists", namespace);
+    }
+  }
+
+  @Override
+  public List<Namespace> listNamespaces(Namespace namespace) throws NoSuchNamespaceException {
+    switch(namespace.levels().length) {
+      case 1:
+        // List all the databases.
+        String catalogName = namespace.level(0);
+        return dbClient.getApi().getCatalog(namespace.level(0)).getDatabases()
+          .stream().map(db -> Namespace.of(catalogName, db)).collect(Collectors.toList());
+      default:
+        throw new NoSuchNamespaceException("Only database level namespaces are supported: %s", namespace);
+    }
+  }
+
+  @Override
+  public Map<String, String> loadNamespaceMetadata(Namespace namespace) throws NoSuchNamespaceException {
+    Preconditions.checkArgument(namespace.levels().length == 2,
+            "Invalid database name: %s", namespace);
+    try {
+      String catalogName = namespace.level(0);
+      String dbName = namespace.level(1);
+      DatabaseDto databaseDto = dbClient.getApi().getDatabase(catalogName, dbName,
+        true /* Include user-metadata */,
+        false /* Include table names */);
+
+      return ImmutableMap.copyOf(databaseDto.getMetadata());
+    } catch (MetacatNotFoundException e) {
+      throw new NoSuchNamespaceException("Namespace: %s not found.", namespace);
+    }
+  }
+
+  @Override
+  public boolean dropNamespace(Namespace namespace) throws NamespaceNotEmptyException {
+    Preconditions.checkArgument(namespace.levels().length == 2,
+            "Invalid database name: %s", namespace);
+
+    try {
+      dbClient.getApi().deleteDatabase(namespace.level(0), namespace.level(1));
+      return true;
+    } catch (MetacatNotFoundException e) {
+      return false;
+    }
+  }
+
+  @Override
+  public boolean setProperties(Namespace namespace, Map<String, String> properties) throws NoSuchNamespaceException {
+    Preconditions.checkArgument(namespace.levels().length == 2,
+            "Invalid database name: %s", namespace);
+
+    HashMap<String, String> metadata = new HashMap<>(loadNamespaceMetadata(namespace));
+    for (Map.Entry<String, String> entry : properties.entrySet()) {
+      if (DB_LOCATION.equals(entry.getKey())) {
+        // TODO: See if we can update metacat to support this.
+        throw new UnsupportedOperationException("Metacat doesn't support setting database location");
+      } else {
+        metadata.put(entry.getKey(), entry.getValue());
+      }
+    }
+
+    try {
+      DatabaseCreateRequestDto createRequestDto = DatabaseCreateRequestDto.builder()
+        .metadata(ImmutableMap.copyOf(metadata))
+        .build();
+      dbClient.getApi().updateDatabase(namespace.level(0), namespace.level(1), createRequestDto);
+      return true;
+    } catch (MetacatNotFoundException e) {
+      throw new NoSuchNamespaceException("Namespace: %s not found.", namespace);
+    }
+  }
+
+  @Override
+  public boolean removeProperties(Namespace namespace, Set<String> properties) throws NoSuchNamespaceException {
+    Preconditions.checkArgument(namespace.levels().length == 2,
+            "Invalid database name: %s", namespace);
+
+    HashMap<String, String> metadata = new HashMap<>(loadNamespaceMetadata(namespace));
+    Optional.ofNullable(properties).orElse(Collections.emptySet()).stream()
+      .forEach(p -> metadata.remove(p));
+
+    try {
+      DatabaseCreateRequestDto createRequestDto = DatabaseCreateRequestDto.builder()
+        .metadata(ImmutableMap.copyOf(metadata))
+        .build();
+      dbClient.getApi().updateDatabase(namespace.level(0), namespace.level(1), createRequestDto);
+      return true;
+    } catch (MetacatNotFoundException e) {
+      throw new NoSuchNamespaceException("Namespace: %s not found.", namespace);
+    }
   }
 }
